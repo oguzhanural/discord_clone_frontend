e016f8eefd846bf580c25e01be8af4ba59d2a515
oguzhanural <oguzhnural@gmail.com>
2024-03-26 15:58:17 +0300

    zustand state menage

---
+++ src/stores/generalStore.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export type Modal = "CreateServer";
+
+interface GeneralStore  {
+    activeModal: Modal | null
+    setActiveModal: (modal: Modal | null ) => void 
+}
+
+export const useGeneralStore = create<GeneralStore>()(
+    persist(
+        //initializer:
+        (set)=>({
+            activeModal: null,
+            setActiveModal: (modal: Modal | null) => set({activeModal: modal}), 
+        }),
+        //options:
+        {
+            name: "general-store",
+        }
+    )
+)
+
